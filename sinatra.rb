371e52123cf6007350c163e0879e51eea9c394bc
Martin Zittel <martin.zittel@gmail.com>
2013-09-09 17:06:01 +0200

    corrected sinatra.rb

--- sinatra.rb
+++ sinatra.rb
@@ -1,24 +1,11 @@
 require 'sinatra'
 require 'haml'
 
-require_relative 'pca9685'
-
+require_relative './settings'
 
 set :port, 8080
 set :bind, '0.0.0.0'
 
-
-DEBUG = true
-
-$color = [0,0,0]
-trace_var :$color, proc{setCubeColor()}
-
-$cube = PCA9685.new(0x40, DEBUG)
-
-# Initialize LEDs
-for i in (0..15)
-	$cube.setPWM(i, 0, 4095)
-end
 
 
 get '/' do
@@ -33,44 +20,7 @@ get '/' do
 end
 
 
-
-def validColors(r,g,b)
-	if(r != nil && g != nil && b != nil)
-		r = r.to_i()
-		g = g.to_i()
-		b = b.to_i()
-		if((0..255).member?(r) && (0..255).member?(g) && (0..255).member?(b))
-			$color = [r,g,b]
-			return true
-		end
-	end
-	return false
-end
-
-def setCubeColor()
-	r = (255 - $color[0])*16 + 15
-	g = (255 - $color[1])*16 + 15
-	b = (255 - $color[2])*16 + 15
-	
-	if DEBUG
-		p r
-		p g
-		p b
-	end
-
-	$cube.setPWM(0, 0, r)
-	$cube.setPWM(1, 0, g)
-	$cube.setPWM(2, 0, b)
-	
-	$cube.setPWM(4, 0, r)
-	$cube.setPWM(5, 0, g)
-	$cube.setPWM(6, 0, b)
-
-	$cube.setPWM(8, 0, r)
-	$cube.setPWM(9, 0, g)
-	$cube.setPWM(10, 0, b)
-
-	$cube.setPWM(12, 0, r)
-	$cube.setPWM(13, 0, g)
-	$cube.setPWM(14, 0, b)
-end
+# pid = spawn("ruby ./plugin.rb")
+# Process.detach(pid)
+# 
+# Process.kill("KILL", pid)
